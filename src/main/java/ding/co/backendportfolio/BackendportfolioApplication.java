b2fbcaf02ea178d4eec01e2faf6f4be36bc8fb18
pryeok <pryeok@gmail.com>
2025-07-29 22:30:49 +0900

    update

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
-        System.out.println("nono!!");
+        System.out.println("good!!");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
