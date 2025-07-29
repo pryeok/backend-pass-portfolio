485bb13ede2fb1a5fc0d6f919b3f72dc3a771085
ryeokpark <ryeokpark@aideep.ai>
2025-07-29 14:59:07 +0900

    update terraform

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
+        System.out.println("2222");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
