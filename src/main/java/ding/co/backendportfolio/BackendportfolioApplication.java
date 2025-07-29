ff4a615bcc153eac2a35b6d2243c4e11e5e20ad7
aideepmobile <info@aideep.ai>
2025-07-29 13:59:40 +0900

    update terraform

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
-        System.out.println("wow!!");
+        System.out.println("wow!!111");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
