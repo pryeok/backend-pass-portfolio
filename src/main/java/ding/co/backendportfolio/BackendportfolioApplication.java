1a19733a10855702b64127087c6ce81a0b282a07
aideepmobile <info@aideep.ai>
2025-07-30 08:43:46 +0900

    terraform commit test

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
-        System.out.println("yes!!");
+        System.out.println("goodgood!!");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
