82c9aa684f57fc5d258dcdd110f53d33fb99a958
pryeok <pryeok@gmail.com>
2025-09-02 22:42:50 +0900

    stream filter

--- src/main/java/ding/co/backendportfolio/chapter5/_3_data_processing/FilterService.java
+++ src/main/java/ding/co/backendportfolio/chapter5/_3_data_processing/FilterService.java
@@ -18,6 +18,7 @@ public class FilterService {
      * 2. 부정적 단어 포함 체크: 비용 10, 선택도 0.33 => FilterOverhead 3.3
      * 3. 포스트 타입 체크: 비용 1, 선택도 0.33 => FilterOverhead 0.33
      */
+   // 전체 게시물 중, 긍정적 감정 분석 결과로 나왔짐나 텍스트에는 부정어가 포함된 비디오 게시물
     public void badFilter(List<Post> posts) {
         posts.stream()
                 .filter(post -> sentimentAnalysisApiClient.isPositiveSentiment(post))
--- src/test/java/ding/co/backendportfolio/config/TestDatabaseConfig.java
+++ src/test/java/ding/co/backendportfolio/config/TestDatabaseConfig.java
@@ -23,7 +23,7 @@ public class TestDatabaseConfig {
         String originalJdbcUrl = mysqlContainer.getJdbcUrl() + "?rewriteBatchedStatements=true";
 
         // TODO: BulkInsert 모니터링 - 아래 주석을 해제해야함
-//        originalJdbcUrl = originalJdbcUrl + "&profileSQL=true&logger=Slf4JLogger&maxQuerySizeToLog=2147483647";
+        originalJdbcUrl = originalJdbcUrl + "&profileSQL=true&logger=Slf4JLogger&maxQuerySizeToLog=2147483647";
 
         System.setProperty("spring.datasource.url", originalJdbcUrl);
         System.setProperty("spring.datasource.username", mysqlContainer.getUsername());
